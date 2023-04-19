f1919db905714ff07cbf68a5ac3aa023e6322504
Mario Krajchevski <krajchevskimario@hotmail.com>
2023-04-19 22:09:21 +0200

    od cas, not tested

---
+++ c06/index.js
@@ -0,0 +1,53 @@
+const config = require('./pkg/config/')
+const express = require('express');
+var { expressjwt: jwt } = require('express-jwt');
+require('./pkg/db');
+const {
+    login,
+    register,
+    refreshToken,
+    forgotPassword,
+    resetPassword
+} = require('./handlers/auth');
+
+const api = express();
+
+api.use(express.json());
+
+api.use(
+    jwt({ secret: config.get('development').jwt_key, algorithms: ['HS256'],
+}).unless({
+    path: [
+        '/api/v1/auth/login',
+        '/api/v1/auth/register',
+        '/api/v1/auth/forgot-password',
+        '/api/v1/auth/reset-password'
+    ]
+})
+);
+
+api.post('/api/v1/auth/login', login);
+api.post('/api/v1/auth/register', register);
+api.post('/api/v1/auth/forgot-password', forgotPassword);
+api.post('/api/v1/auth/reset-password', resetPassword);
+
+api.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedAccess') {
+        res.status(401).send('Invalid token...');
+    }
+});
+
+//GET, POST, PUT, PATCH, DELETE
+
+//if(config.get('development').jwt_key)
+
+
+api.listen(config.get('development').port, (err) => {
+    err ? console.log(err) : console.log(`Server started on ${config.get('development').port}`);
+});
+
+// homework from 4/19/2023
+// 1. forgot password - try to implement it with mailgun
+// 2. test out reset password
+// 3. read about express-jwt lib
+// 4. test out all the routes and if there are errors try to debug them
